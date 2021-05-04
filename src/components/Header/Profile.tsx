44705a794b8b2b3bd0f0c7e0e143e975e4c658bb
Bruno Fray <brunofray@gmail.com>
2021-05-04 18:18:54 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
+      { showProfileData && (
       <Box mr="4" textAlign="right">
         <Text>Bruno Fray</Text>
         <Text color="gray.300" fontSize="small">
           brunofray@gmail.com
         </Text>
       </Box>
+      )}
 
       <Avatar size="md" name="Bruno Fray" src="https://github.com/brunofray.png"/>
     </Flex>
